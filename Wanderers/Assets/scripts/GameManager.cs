a5c6cbbdbf52d755cce5cda6b762b65ced0fc184
Prisyafandiafif Charifa <prisyafandiafif.charifa@gmail.com>
2016-11-26 17:40:16 -0500

    merge

---
+++ Wanderers/Assets/scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameManager : MonoBehaviour 
+{
+	public GameObject normalFullScreenTransitionGameObject;
+
+	public GameObject playerPageGameObject;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void OnPortraitButtonClicked ()
+	{
+		//play fullscreen animation
+		StartCoroutine(PlayNormalFullScreenTransitionAnimation(ShowPlayerPage));
+	}
+
+	public IEnumerator PlayNormalFullScreenTransitionAnimation (Action functionToCall)
+	{
+		normalFullScreenTransitionGameObject.SetActive(true);
+
+		normalFullScreenTransitionGameObject.transform.FindChild("Animation").gameObject.GetComponent<Animator>().Play("start");
+
+		yield return new WaitForSeconds(0.4f);
+
+		functionToCall();
+
+		normalFullScreenTransitionGameObject.transform.FindChild("Animation").gameObject.GetComponent<Animator>().Play("end");
+
+		normalFullScreenTransitionGameObject.SetActive(false);
+	}
+
+	public void ShowPlayerPage ()
+	{
+		playerPageGameObject.SetActive(true);
+	}
+}
